c18957fa7b73a1fd6c23c8f42fabaea246059dd8
BentoTheScientist <144745780+BentoTheScientist@users.noreply.github.com>
2024-07-23 08:58:57 -0300

    Add files via upload
    Exercício da disciplina de CAP

---
+++ exercicio_de_soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b, x;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    x = a+b;
+
+    printf("X = %d\n", x);
+
+    return  0;
+}
