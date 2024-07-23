311701077734eb8d0e557efdc1b3bb581e2045a0
BentoTheScientist <144745780+BentoTheScientist@users.noreply.github.com>
2024-07-23 11:03:27 -0300

    Add files via upload
    Exercício de produtos simples

---
+++ produto_simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    
+    int a,b;
+    int PROD;
+
+    scanf("%d %d", &a, &b);
+    PROD = a*b;
+
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
